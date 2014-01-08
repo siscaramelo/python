ea6ce0819a7a79521a5641f4279f98f03e81b5ef
siscaramelo <pgiraldez@caramelo.com>
2014-01-08 15:50:14 +0100

    Add gradient descent function

--- linearRegression/__init__.py
+++ linearRegression/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-Created on 13/05/2013
+Created on 08/01/2014
 
 @author: pabgir
 '''
@@ -17,4 +17,21 @@ def computeCost(X, y, theta):
     m           = np.size(y)     # Number of training examples
     
     # The linear regression cost function
-    computeCost = 1/(2*m)*(np.dot(np.transpose(np.dot(X,theta)-y),(np.dot(X,theta)-y)))
+    computeCost = 1/(2*m)*(np.dot(np.transpose(np.dot(X,theta)-y),np.dot(X,theta)-y))
+    
+
+def gradientDescent(X, y, theta, alpha, num_iters):    
+    
+    #
+    #    Performs gradient descent to learn theta
+    #
+    
+    m           = np.size(y)     # Number of training examples
+    J_history   = np.zeros((num_iters,1))   # Inicializamos el vector de datos historicos
+    
+    for i in range(num_iters):
+     
+        # theta = theta - alpha/m*X'*(X*theta-y)
+        theta = theta - alpha*(np.dot(np.transpose(X),np.dot(X,theta)-y))/m
+
+        J_history[i] = computeCost(X, y, theta)
