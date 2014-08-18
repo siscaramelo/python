91f919c223d3122311c35cc7a3e80705f7ff9c3d
siscaramelo <pgiraldez@caramelo.com>
2014-08-18 15:57:04 +0200

    Centroides

--- Clustering/__init__.py
+++ Clustering/__init__.py
@@ -6,6 +6,8 @@ Created on 08/01/2014
 '''
 from numpy import *
 from numpy import newaxis, r_, c_, mat, e
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
 
 def findClosestCentroids(X,centroids):
     # Returns the closest centroids for a dataset X where each row is a single example
@@ -53,3 +55,56 @@ def computeCentroids(X,idx,K):
     
     return centroids
 
+def runkMeans(X, initial_centroids, max_iters, plot_progress=False):
+    # Initialize values
+    m,n                 = shape(X)
+    K                   = size(initial_centroids, 0)
+    centroids           = initial_centroids
+    previous_centroids  = centroids
+    idx                 = zeros([m, 1])
+    
+    # Run K-Means
+    for i in range(max_iters):
+    
+        # Output progress
+        print('K-Means iteration ...', i+1, max_iters)
+    
+        # For each example in X, assign it to the closest centroid
+        idx = findClosestCentroids(X, centroids)
+    
+        # Optionally, plot progress here
+        if plot_progress:
+            plotProgresskMeans(X, centroids, previous_centroids, idx, K, i)
+            previous_centroids = centroids
+            print('Press enter to continue...\n')
+            #pause;
+    
+            # Given the memberships, compute new centroids
+            centroids = computeCentroids(X, idx, K)
+    
+    return centroids, idx
+
+
+def plotDataPoints(X, idx, K):
+    
+    plt.scatter(X[:,0],X[:,1], s=15)
+    #plt.show()
+    
+    return
+
+def plotProgresskMeans(X, centroids, previous, idx, K, i):
+    
+    # Plot the examples
+    plotDataPoints(X, idx, K);
+
+    # Plot the centroids as red dots
+    plt.plot(centroids[:,0], centroids[:,1], 'ro') #'r', marker='x', markersize=10,  linewidth=0) #
+
+    # Plot the history of the centroids with lines
+    for j in range(size(centroids,0)):
+        plt.plot([centroids[j,0], previous[j,0]],[centroids[j,1], previous[j,1]],'g-')
+        
+    # Title
+    plt.title('Iteration number: %d' % (i+1))
+    plt.show()
+    return
--- Test_K-Means.py
+++ Test_K-Means.py
@@ -61,3 +61,33 @@ if __name__ == '__main__':
     print('   [ 7.119387 3.616684 ]\n')
 
     raw_input('Program paused 2. Press any key to continue\n')
+    
+    ## =================== Part 3: K-Means Clustering ======================
+    #  After you have completed the two functions computeCentroids and
+    #  findClosestCentroids, you have all the necessary pieces to run the
+    #  kMeans algorithm. In this part, you will run the K-Means algorithm on
+    #  the example dataset we have provided. 
+    #
+    
+    print('\nRunning K-Means clustering on example dataset.\n\n')
+
+    # Load an example dataset
+    data = loadmat('C:\Users\pgiraldez\Documents\Octave\mlclass-ex7\ex7data2.mat') 
+    X     = data['X'] 
+
+    # Settings for running K-Means
+    K = 3
+    max_iters = 10
+
+    # For consistency, here we set centroids to specific values
+    # but in practice you want to generate them automatically, such as by
+    # settings them to be random examples (as can be seen in
+    # kMeansInitCentroids).
+    initial_centroids = array([[3.,3.],[6.,2.],[8.,5.]])
+
+    # Run K-Means algorithm. The 'true' at the end tells our function to plot
+    # the progress of K-Means
+    centroids, idx = runkMeans(X, initial_centroids, max_iters, True)
+    print('\nK-Means Done.\n\n')
+
+    raw_input('Program paused 3. Press any key to continue\n')
